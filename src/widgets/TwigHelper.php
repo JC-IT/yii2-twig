695bd2102daa8fee920025c266ba2463aa08ce98
Joey Claessen <joey_claessen@hotmail.com>
2023-05-24 11:48:30 +0000

    feat: added bootstrap 5 and allow for version configuration

--- src/widgets/TwigHelper.php
+++ src/widgets/TwigHelper.php
@@ -9,7 +9,10 @@ use yii\helpers\Html;
 
 class TwigHelper extends \yii\base\Widget
 {
+    public string $bootstrap4Version = '4.6';
+    public string $bootstrap5Version = '5.3';
     public bool $hasBootstrap4 = true;
+    public bool $hasBootstrap5 = false;
     public TwigHelperInterface $helper;
 
     protected function renderMethods(): string
@@ -30,13 +33,16 @@ class TwigHelper extends \yii\base\Widget
         $result .= Html::beginTag('div', ['class' => ['callout', 'callout-info', 'mt-3']]);
         $result .= Html::tag('p', \Yii::t('app', 'In the editor above you can create content with advanced mechanics:'));
         $result .= Html::ul(array_filter([
-            ($this->hasBootstrap4) ? Html::a(\Yii::t('app', 'Bootstrap 4'), 'https://getbootstrap.com/docs/4.6/', ['target' => '_blank']) : null,
+            ($this->hasBootstrap4) ? Html::a(\Yii::t('app', 'Bootstrap 4'), "https://getbootstrap.com/docs/{$this->bootstrap4Version}/", ['target' => '_blank']) : null,
+            ($this->hasBootstrap5) ? Html::a(\Yii::t('app', 'Bootstrap 5'), "https://getbootstrap.com/docs/{$this->bootstrap5Version}/", ['target' => '_blank']) : null,
             Html::a(\Yii::t('app', 'Twig 3'), 'https://twig.symfony.com/doc/3.x/templates.html', ['target' => '_blank']),
         ]), ['encode' => false]);
         $result .= Html::tag('p', \Yii::t('app', 'While this approach is very powerful, it also requires some knowledge. If you can\'t get something to work, please contact us.'));
-        $result .= Html::tag('p', \Yii::t('app', 'To provide flexibility and integration with there is a helper available in Twig "code". Please read the Twig documentation how to use the object, it\'s named <code>helper</code> and provides the following methods:'));
+        if (!empty($this->helper::methodDescriptions())) {
+            $result .= Html::tag('p', \Yii::t('app', 'To provide flexibility and integration with there is a helper available in Twig "code". Please read the Twig documentation how to use the object, it\'s named <code>helper</code> and provides the following methods:'));
 
-        $result .= $this->renderMethods();
+            $result .= $this->renderMethods();
+        }
 
         $result .= Html::endTag('div');
         return $result;
